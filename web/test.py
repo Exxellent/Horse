0deedf90c2437a6a61f919cd5e0965822180b295
Exxellent <mikimiki55550@gmail.com>
2022-11-24 15:06:06 +0000

    correct test.py

--- web/test.py
+++ web/test.py
@@ -5,6 +5,7 @@ os.system('bandit -r ./app/ -o bandit.json -f json')
 
 with open('bandit.json', 'r') as f:
     b = json.load(f)
-
-if b['results'] or pylint:
+if pylint:
+    print("Ошибки качества кода")
+if b['results']:
     raise RuntimeError('Ошибки')
