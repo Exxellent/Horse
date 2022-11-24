8ac4c54b4fdc5f475bb6bdaf3ffc669181f1a97e
Exxellent <mikimiki55550@gmail.com>
2022-11-24 15:39:29 +0000

    correct test.py

--- web/test.py
+++ web/test.py
@@ -4,7 +4,7 @@ pylint = os.popen('pylint ./app').read()
 if pylint:
     print("Ошибки качества кода")
 
-os.system('bandit -r ./app/ -o bandit.json -f json')
+os.system('bandit -r ./app/ -o bandit.json -f json -lll -iii -q')
 with open('bandit.json', 'r') as f:
     b = json.load(f)
 if b['results']:
