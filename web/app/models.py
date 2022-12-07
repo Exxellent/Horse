e84ba2e02949c8b63077d995867e2a663ac852f6
Exxellent <mikimiki55550@gmail.com>
2022-12-07 12:50:11 +0300

    fix

--- web/app/models.py
+++ web/app/models.py
@@ -8,9 +8,9 @@ from app import db
 class User(db.Model, UserMixin):  
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    login = Column(String)
-    password = Column(String)
+    id = db.Column(db.Integer, primary_key=True)    
+    login = db.Column(db.String(40), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
     
     def set_password(self, password: str):
         self.password = generate_password_hash(password)
@@ -19,4 +19,11 @@ class User(db.Model, UserMixin):
     def check_password(self, password: str):
         return check_password_hash(self.password, password)
     
+class Horse(db.Model):
+    __tablename__ = 'horses'
+    
+    id = db.Column(db.Integer, primary_key=True)    
+    name = db.Column(db.String(50), nullable=False)
+    
+
 
