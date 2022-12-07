46d6e10856e71fea61f5f1305faccaf58144f957
Exxellent <mikimiki55550@gmail.com>
2022-12-07 07:52:57 +0000

    test

--- web/test.py
+++ web/test.py
@@ -8,4 +8,4 @@ os.system('bandit -iii -lll -q -r ./app/ -o bandit.json -f json ')
 with open('bandit.json', 'r') as f:
     b = json.load(f)
 if b['results']:
-    raise RuntimeError('Ошибки')
+    raise RuntimeError('Ошибки учетных данных')
