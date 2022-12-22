239912bb2af0947bca871b6fe1dd9ed60d505e6d
Exxellent <71991266+Exxellent@users.noreply.github.com>
2022-12-22 11:53:52 +0300

    Update models.py

--- web/app/models.py
+++ web/app/models.py
@@ -3,8 +3,6 @@ import sqlalchemy as sa
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_login import UserMixin
 
-
-
 from app import db, app
 
 class User(db.Model, UserMixin):  
