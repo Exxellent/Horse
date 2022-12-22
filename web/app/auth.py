49b0fd1c8be7fcdfdca60610f27e472bf616c9fd
Exxellent <71991266+Exxellent@users.noreply.github.com>
2022-12-22 20:56:35 +0300

    Update auth.py
    fix app.py

--- web/app/auth.py
+++ web/app/auth.py
@@ -44,7 +44,7 @@ def login():
                 login_user(user)
                 flash('Вы успешно аутентифицированы.', 'success')
                 next = request.args.get('next')
-                return render_template('auth/login.html')
+                return redirect(next or url_for('index'))
         flash('Неправильный логин или пароль','danger')
     return render_template('auth/login.html')
 
