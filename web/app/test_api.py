d477e174b954e20fc770c0f07bc58f8d3c77abcb
Ренат <71991266+Exxellent@users.noreply.github.com>
2023-03-10 16:58:54 +0300

    Update test_api.py

--- web/app/test_api.py
+++ web/app/test_api.py
@@ -11,13 +11,13 @@ def test_login():
     assert response.status_code == 200
 
 def test_valid_auth():
-    data={"login": "renat",
-          "password" : "renat"}
+    data={"login": "silwar",
+          "password" : "admin"}
     response = client.post('/auth/login', data=data)
     assert response.status_code == 302
 
 def test_invalid_auth():
-    data={"login": "renat",
+    data={"login": "silwar",
           "password" : "user"}
     response = client.post('/auth/login', data=data)
     assert response.status_code == 200
